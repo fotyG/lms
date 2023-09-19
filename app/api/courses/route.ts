52098f03566b65744ab6d67a6a6d55a23f34c64f
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-19 21:07:47 +0200

    add: icon badge components with variants and size props using cva add: create course api route

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
@@ -9,6 +10,15 @@ export async function POST(req: Request) {
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const course = await db.course.create({
+      data: {
+        title,
+        userId,
+      },
+    });
+
+    return NextResponse.json(course);
   } catch (error) {
     console.log("[COURSES]", error);
     return new NextResponse("Internal Error", { status: 500 });
