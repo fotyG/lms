b34fad379f6896ab6d4c6d4846357fee8bf7803c
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-26 13:02:32 +0200

    add: attachment form and routes

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,10 @@
+import {
+  File,
+  ListChecks,
+  LayoutDashboard,
+  CircleDollarSign,
+} from "lucide-react";
 import { redirect } from "next/navigation";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
 
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
@@ -7,9 +12,10 @@ import { IconBadge } from "@/components/icon-badge";
 
 import TitleForm from "./_components/title-form";
 import ImageForm from "./_components/image-form";
+import PriceForm from "./_components/price-form";
 import CategoryForm from "./_components/category-form";
 import DescriptionForm from "./_components/description-form";
-import PriceForm from "./_components/price-form";
+import AttachmentForm from "./_components/attachment-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -21,6 +27,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+    },
+    include: {
+      attachments: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
     },
   });
 
@@ -89,6 +102,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               <h2 className="text-xl">Sell your course</h2>
             </div>
             <PriceForm initialData={course} courseId={course.id} />
+          </div>
+
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <h2 className="text-xl">Resources & Attachments</h2>
+            </div>
+            <AttachmentForm courseId={course.id} initialData={course} />
           </div>
         </div>
       </div>
---
+++ app/api/courses/[courseId]/attachments/route.ts
@@ -0,0 +1,41 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { courseId: string } },
+) {
+  try {
+    const { userId } = auth();
+    const { url } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        userId,
+        id: params.courseId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const attachment = await db.attachment.create({
+      data: {
+        url,
+        courseId: params.courseId,
+        name: url.split("/").pop(),
+      },
+    });
+
+    return NextResponse.json(attachment);
+  } catch (error) {
+    console.log("[COURSE_ID_ATTACHMENTS]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
